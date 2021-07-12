f1130b178050558f562891df33d3ed4451a5a4c8
Bloubloublou <benedicte@bodyguard.ai>
2021-07-12 15:05:05 +0200

    Initial Commit

---
+++ WeatherTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherTraining
+//
+//  Created by Bénédicte Lagouge on 12/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
